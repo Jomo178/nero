817733f493af01db3db8f9ff8992400420018c9c
Jomo <76564080+Jomo178@users.noreply.github.com>
2023-06-18 11:57:03 +0000

    image

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,28 @@
+import Image from 'next/image'
+
 function Navbar() {
   return (
     <>
-      <nav className="">hello</nav>
+      <nav className="">
+        <div>
+          <ImageAvatar src={bot.imageUrl}></ImageAvatar>
+        </div>
+      </nav>
     </>
   );
 }
+
+function ImageAvatar({src}:{src: string}) {
+  return (<>
+  <div className='rounded'>
+    <Image src={src} alt='avatar' width={40} height={40}></Image>
+  </div>
+  </>)
+}
+
+const bot ={
+  imageUrl: "https://cdn.discordapp.com/avatars/542770757382569994/fad5207a33644450890312e26a2183fb.webp?size=128",
+  name: "Nero"
+} 
 
 export default Navbar;
