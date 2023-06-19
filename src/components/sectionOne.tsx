01d78637d42d29d126ffa188334aacfb670adfc1
Jomo178 <shahbazi8@outlook.de>
2023-06-19 14:02:13 +0200

    section

--- src/components/sectionOne.tsx
+++ src/components/sectionOne.tsx
@@ -1,8 +1,11 @@
+"use client";
+import { motion } from "framer-motion";
+
 function SectionOne() {
   return (
     <>
-      <section className="flex gap-2 justify-between mx-4 items-center min-h-screen">
-        <Cards></Cards>
+      <section className="flex gap-2 justify-between mx-4 items-center min-h-screen w-full relative">
+        <Cards animation={{ x: 100 }}></Cards>
         <div>
           <h3>Nero</h3>
           <p>Lorem ipsum dolor sit amet consectetur.</p>
@@ -11,14 +14,19 @@ function SectionOne() {
             <button>Join Server</button>
           </div>
         </div>
-        <Cards></Cards>
+        <Cards animation={{ x: 100 }}></Cards>
       </section>
     </>
   );
 }
 
-function Cards({ image }: { image?: string }) {
-  return <div className="bg-white rounded-lg shadow-md p-4 w-44 h-72"></div>;
+function Cards({ image, animation }: { image?: string; animation: object }) {
+  return (
+    <motion.div
+      animate={animation}
+      className="bg-white rounded-lg shadow-md p-4 w-44 h-72 absolute"
+    ></motion.div>
+  );
 }
 
 export default SectionOne;
