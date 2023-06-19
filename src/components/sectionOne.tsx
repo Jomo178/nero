8d9c313dae57237a760af0e3af45019e524c083e
Jomo <76564080+Jomo178@users.noreply.github.com>
2023-06-19 13:22:31 +0000

    section

--- src/components/sectionOne.tsx
+++ src/components/sectionOne.tsx
@@ -4,9 +4,15 @@ import { motion } from "framer-motion";
 function SectionOne() {
   return (
     <>
-      <section className="flex gap-2 justify-between mx-4 items-center min-h-screen w-full relative">
-        <Cards animation={{ x: 100 }}></Cards>
-        <div>
+      <section className="flex gap-2 justify-between mx-4 items-center min-h-screen w-full relative overflow-x-hidden">
+        <div className="min-w-[50px] h-full">
+        <motion.div
+      animate={{y: 120, x:120}}
+      transition={{ duration: 4 }}
+      className="bg-white rounded-lg shadow-md p-4 w-44 h-72 absolute -top-11"
+    ></motion.div>
+        </div>
+        <div className="flex flex-col gap-2">
           <h3>Nero</h3>
           <p>Lorem ipsum dolor sit amet consectetur.</p>
           <div>
@@ -14,7 +20,12 @@ function SectionOne() {
             <button>Join Server</button>
           </div>
         </div>
-        <Cards animation={{ x: 100 }}></Cards>
+        <div className="min-w-[50px] h-full">
+        <motion.div
+      // animate={animation}
+      className="bg-white rounded-lg shadow-md p-4 w-44 h-72 absolute -top-11 right-8"
+    ></motion.div>
+        </div>
       </section>
     </>
   );
@@ -24,7 +35,7 @@ function Cards({ image, animation }: { image?: string; animation: object }) {
   return (
     <motion.div
       animate={animation}
-      className="bg-white rounded-lg shadow-md p-4 w-44 h-72 absolute"
+      className="bg-white rounded-lg shadow-md p-4 w-44 h-72 absolute top-9"
     ></motion.div>
   );
 }
