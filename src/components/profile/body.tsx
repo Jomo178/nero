de1b444ac3cbe570ade98f0d914d8a6fe596579a
Jomo178 <shahbazi8@outlook.de>
2023-07-05 23:03:16 +0200

    profile sextion two

--- src/components/profile/body.tsx
+++ src/components/profile/body.tsx
@@ -1,27 +1,129 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import { useState } from "react";
+import {
+  MouseEventHandler,
+  MutableRefObject,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 function ProfileBody() {
+  const divRef = useRef<HTMLDivElement>(null);
+  const firstItemRefs = useRef<
+    Array<MutableRefObject<HTMLParagraphElement | null>>
+  >([]);
+  const [selectedItem, setSelectedItem] = useState<number>(1);
+
+  let initialPosition: DOMRect | null = null;
+
+  useEffect(() => {
+    const element = firstItemRefs.current[0];
+
+    if (element) {
+      //@ts-ignore
+      initialPosition = element.getBoundingClientRect();
+      //@ts-ignore
+      const { left, top, width } = initialPosition;
+
+      if (divRef.current) {
+        divRef.current.style.left = `${left - 20}px`;
+        divRef.current.style.width = `${width - 20}px`;
+      }
+
+      console.log("Initial position:", initialPosition);
+    }
+  }, []);
+
+  const handleMouseEnter: MouseEventHandler<HTMLParagraphElement> = (event) => {
+    const element = event.target as HTMLParagraphElement;
+    const rect = element.getBoundingClientRect();
+    const { left, top, width } = rect;
+
+    if (divRef.current) {
+      divRef.current.style.transform = `translateX(-${width}px)`;
+      divRef.current.style.width = `${width}px`;
+    }
+
+    console.log("Element position:", { rect });
+  };
+
+  const handleClick: MouseEventHandler<HTMLParagraphElement> = (event) => {
+    const element = event.target as HTMLParagraphElement;
+    const index = parseInt(element.dataset.index || "");
+
+    setSelectedItem(index);
+  };
+
   return (
     <>
       <section className="mt-82">
-        <div className="flex gap-8 justify-center pt-8">
-          <p className="p-4 rounded-md">User Info</p>
-          <p className="p-4 rounded-md">Favourite Cards</p>
-          <p className="p-4 rounded-md">Wishlist</p>
+        <div className="flex gap-8 pt-8 relative cursor-pointer">
+          <ProfileTitle
+            text="User Info"
+            onMouseEnter={handleMouseEnter}
+            onClick={handleClick}
+            index={1}
+            selected={selectedItem}
+            ref={firstItemRefs.current[0]}
+          ></ProfileTitle>
+          <ProfileTitle
+            text="Favourite Cards"
+            onMouseEnter={handleMouseEnter}
+            onClick={handleClick}
+            index={2}
+            selected={selectedItem}
+            ref={(el) => (el != null ? el : firstItemRefs.current[0])}
+          ></ProfileTitle>
+          <ProfileTitle
+            text="Wishlist"
+            onMouseEnter={handleMouseEnter}
+            onClick={handleClick}
+            index={3}
+            selected={selectedItem}
+            ref={firstItemRefs.current[2]}
+          ></ProfileTitle>
+          <div
+            ref={divRef}
+            style={{ width: "118px", transform: "translateX(0px)" }}
+            className="absolute h-1 bottom-0 bg-black transition-all duration-500"
+          ></div>
         </div>
+
         <div className="text-black mt-9">
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
             iste ratione totam, earum ducimus temporibus nihil inventore
             provident velit dicta facilis tempora explicabo sit veniam eveniet
             ut consectetur magnam quisquam atque minus cum excepturi dolores.
-            Tempora, ut doloribus. Incidunt, laborum.
+            Tempora
           </p>
         </div>
       </section>
     </>
   );
 }
+
+const ProfileTitle = forwardRef<
+  HTMLParagraphElement,
+  {
+    text: string;
+    onMouseEnter: MouseEventHandler<HTMLParagraphElement>;
+    onClick: MouseEventHandler<HTMLParagraphElement>;
+    index: number;
+    selected: number;
+  }
+>(({ text, onMouseEnter, onClick, index, selected }, ref) => {
+  return (
+    <p
+      className={`p-4 ${selected === index ? "text-red-500" : ""}`}
+      onMouseEnter={onMouseEnter}
+      onClick={onClick}
+      data-index={index}
+      ref={ref}
+    >
+      {text}
+    </p>
+  );
+});
 
 export default ProfileBody;
