636bb4e2b479716fe497d5c6da93341c17e5884f
Jomo178 <shahbazi8@outlook.de>
2023-07-10 20:04:01 +0200

    fileDirectory

--- src/components/blog/code.tsx
+++ src/components/blog/code.tsx
@@ -3,9 +3,9 @@
 
 import { tw } from "@/src/function/functions";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-const Code = async ({ children, language }) => {
+const Code = async ({ children, language, fileDirectory }) => {
   const languageClassName = tw(
     "relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm"
   );
@@ -15,11 +15,9 @@ const Code = async ({ children, language }) => {
       <pre className={languageClassName}>
         <code>{language}</code>
       </pre>
-      <pre>
-        <SyntaxHighlighter language={language} style={atomDark} showLineNumbers>
-          {children}
-        </SyntaxHighlighter>
-      </pre>
+      <SyntaxHighlighter language={language} style={dracula} showLineNumbers>
+        {children}
+      </SyntaxHighlighter>
     </div>
   );
 };
